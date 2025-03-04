66e79ceee9bbc36350412bc4049a9eb6d4a571f3
V9vik <myzikov03@gmail.com>
2025-03-05 01:17:16 +0300

    Update test + confiq logic

--- internal/confiq/confiq.go
+++ internal/confiq/confiq.go
@@ -2,6 +2,7 @@ package confiq
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -13,24 +14,29 @@ type Config struct {
 
 func LoadConfig() *Config {
 	var cfg Config
-	cfg.Address = os.Getenv("SERVER_ADDRESS")
-	cfg.BaseURL = os.Getenv("BASE_URL")
-	if cfg.Address == "" {
+	flag.StringVar(&cfg.Address, "a", "", "Адрес запуска HTTP-сервера")
+	flag.StringVar(&cfg.BaseURL, "b", "", "Базовый адрес результирующего сокращенного URL")
 
-		flag.StringVar(&cfg.Address, "a", "localhost:8080", "HTTP server address")
-		flag.StringVar(&cfg.BaseURL, "b", "", "Base URL for short links")
-		flag.Parse()
-
-		if cfg.BaseURL == "" {
-			if strings.HasPrefix(cfg.Address, ":") {
-				cfg.BaseURL = "http://localhost%s" + cfg.Address
-			} else {
-				cfg.BaseURL = "http://" + cfg.Address
-			}
-		}
-
-		return &cfg
-	} else {
-		return &cfg
+	flag.Parse()
+	if envAddr := os.Getenv("SERVER_ADDRESS"); envAddr != "" {
+		cfg.Address = envAddr
 	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		cfg.BaseURL = envBaseURL
+	}
+
+	if cfg.Address == "" {
+		cfg.Address = "localhost:8080"
+	}
+
+	if cfg.BaseURL == "" {
+		if strings.HasPrefix(cfg.Address, ":") {
+			cfg.BaseURL = fmt.Sprintf("http://localhost%s", cfg.Address)
+		} else {
+			cfg.BaseURL = fmt.Sprintf("http://%s", cfg.Address)
+		}
+	}
+
+	return &cfg
 }
